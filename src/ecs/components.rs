56b3a19bedd3182023e44cb00a9880af918c3eb2
Russel Arbore <russel.jma@gmail.com>
2022-03-29 16:52:13 -0500

    Added license headers

--- src/ecs/components.rs
+++ src/ecs/components.rs
@@ -1,3 +1,23 @@
-pub struct Test {
-    pub x: f64,
+/*
+ * This file is part of game-testbed.
+ * game-testbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * game-testbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with game-testbed. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+}
+
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
 }
--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -1,3 +1,17 @@
+/*
+ * This file is part of game-testbed.
+ * game-testbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * game-testbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with game-testbed. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 mod components;
 
 pub use components::*;
