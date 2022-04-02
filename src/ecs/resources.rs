75627902b5daa77b4588dd66c6ae13119d2f128c
Russel Arbore <russel.jma@gmail.com>
2022-04-02 08:06:20 -0500

    Generic resource

---
+++ src/ecs/resources.rs
@@ -0,0 +1,34 @@
+/*
+ * This file is part of game-testbed.
+ * game-testbed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * game-testbed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with game-testbed. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use std::time::Instant;
+
+pub trait Resource: Send + Sync {}
+
+pub struct Timer {
+    start: Instant,
+}
+
+impl Timer {
+    pub fn new() -> Self {
+        Self {
+            start: Instant::now(),
+        }
+    }
+    pub fn millis(&self) -> u32 {
+        self.start.elapsed().as_millis()
+    }
+}
+
+impl Resource for Timer {}
