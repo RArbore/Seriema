1a9d15798292e030f104f545d11f0ec7697afb7e
Russel Arbore <russel.jma@gmail.com>
2022-04-14 20:19:23 -0500

    Generate tileset

--- gen-tileset/src/main.rs
+++ gen-tileset/src/main.rs
@@ -18,17 +18,18 @@ use image::imageops::*;
 use image::io::*;
 use image::*;
 
-pub const LEFT_MASK: u8 = 0x01;
-pub const UP_LEFT_MASK: u8 = 0x02;
-pub const UP_MASK: u8 = 0x04;
-pub const UP_RIGHT_MASK: u8 = 0x08;
-pub const RIGHT_MASK: u8 = 0x10;
-pub const DOWN_RIGHT_MASK: u8 = 0x20;
-pub const DOWN_MASK: u8 = 0x40;
-pub const DOWN_LEFT_MASK: u8 = 0x80;
+const LEFT_MASK: u8 = 0x01;
+const UP_LEFT_MASK: u8 = 0x02;
+const UP_MASK: u8 = 0x04;
+const UP_RIGHT_MASK: u8 = 0x08;
+const RIGHT_MASK: u8 = 0x10;
+const DOWN_RIGHT_MASK: u8 = 0x20;
+const DOWN_MASK: u8 = 0x40;
+const DOWN_LEFT_MASK: u8 = 0x80;
 
 #[repr(u8)]
-pub enum QuadEdgeType {
+#[derive(Clone, Copy, Debug)]
+enum QuadEdgeType {
     Clean,
     Diag,
     Horiz,
@@ -37,61 +38,71 @@ pub enum QuadEdgeType {
 }
 
 fn num_to_edges(n: u8) -> [QuadEdgeType; 4] {
-    // n | MASK == 0 means there is not a similar neighbor in the MASK direction
+    // n & MASK == 0 means there is not a similar neighbor in the MASK direction
     let mut ret = [
-        if n | UP_LEFT_MASK == 0 {
+        if n & UP_LEFT_MASK == 0 {
             QuadEdgeType::Diag
         } else {
             QuadEdgeType::Clean
         },
-        if n | UP_RIGHT_MASK == 0 {
+        if n & UP_RIGHT_MASK == 0 {
             QuadEdgeType::Diag
         } else {
             QuadEdgeType::Clean
         },
-        if n | DOWN_LEFT_MASK == 0 {
+        if n & DOWN_LEFT_MASK == 0 {
             QuadEdgeType::Diag
         } else {
             QuadEdgeType::Clean
         },
-        if n | DOWN_RIGHT_MASK == 0 {
+        if n & DOWN_RIGHT_MASK == 0 {
             QuadEdgeType::Diag
         } else {
             QuadEdgeType::Clean
         },
     ];
 
-    if n | LEFT_MASK == 0 {
+    if n & LEFT_MASK == 0 {
         ret[0] = QuadEdgeType::Horiz;
         ret[2] = QuadEdgeType::Horiz;
     }
-    if n | RIGHT_MASK == 0 {
+    if n & RIGHT_MASK == 0 {
         ret[1] = QuadEdgeType::Horiz;
         ret[3] = QuadEdgeType::Horiz;
     }
-    if n | UP_MASK == 0 {
+    if n & UP_MASK == 0 {
         ret[0] = QuadEdgeType::Vert;
         ret[1] = QuadEdgeType::Vert;
     }
-    if n | DOWN_MASK == 0 {
+    if n & DOWN_MASK == 0 {
         ret[2] = QuadEdgeType::Vert;
         ret[3] = QuadEdgeType::Vert;
     }
 
-    if n | LEFT_MASK == 0 && n | UP_MASK == 0 {
+    if n & LEFT_MASK == 0 && n & UP_MASK == 0 {
         ret[0] = QuadEdgeType::Full;
     }
-    if n | RIGHT_MASK == 0 && n | UP_MASK == 0 {
+    if n & RIGHT_MASK == 0 && n & UP_MASK == 0 {
         ret[1] = QuadEdgeType::Full;
     }
-    if n | LEFT_MASK == 0 && n | DOWN_MASK == 0 {
+    if n & LEFT_MASK == 0 && n & DOWN_MASK == 0 {
         ret[2] = QuadEdgeType::Full;
     }
-    if n | RIGHT_MASK == 0 && n | DOWN_MASK == 0 {
+    if n & RIGHT_MASK == 0 && n & DOWN_MASK == 0 {
         ret[3] = QuadEdgeType::Full;
     }
 
     ret
+}
+
+fn quad_to_coord(quad_num: u32, quad_type: QuadEdgeType) -> (u32, u32) {
+    match quad_type {
+        QuadEdgeType::Clean => (16, 16),
+        QuadEdgeType::Diag => (8 * (quad_num % 2), 8 * (quad_num / 2)),
+        QuadEdgeType::Horiz => (8 * (quad_num % 2) + 16, 0),
+        QuadEdgeType::Vert => (8 * (quad_num / 2) + 16, 8),
+        QuadEdgeType::Full => (8 * (quad_num % 2), 8 * (quad_num / 2) + 16),
+    }
 }
 
 fn concat4(quads: [DynamicImage; 4]) -> DynamicImage {
@@ -106,12 +117,26 @@ fn concat4(quads: [DynamicImage; 4]) -> DynamicImage {
 fn main() {
     let args: Vec<String> = args().collect();
     for arg in args[1..].iter() {
+        assert_ne!(arg, &arg.replace("assets/", "assets/gen/"));
         let img = Reader::open(arg)
             .expect(&format!("Couldn't open file: {}.", arg)[..])
             .decode()
             .expect(&format!("File couldn't be coded: {}.", arg)[..]);
-        println!("{:?}", img);
         let mut tileset = RgbaImage::new(256 * 16, 16);
-        for i in 0u8..=255u8 {}
+        for i in 0u8..=255u8 {
+            let edges = num_to_edges(i);
+            let mut coords: [(u32, u32); 4] = [(0, 0), (0, 0), (0, 0), (0, 0)];
+            for j in 0..4 {
+                coords[j] = quad_to_coord(j as u32, edges[j]);
+            }
+            let constructed = concat4([
+                img.crop_imm(coords[0].0, coords[0].1, 8, 8),
+                img.crop_imm(coords[1].0, coords[1].1, 8, 8),
+                img.crop_imm(coords[2].0, coords[2].1, 8, 8),
+                img.crop_imm(coords[3].0, coords[3].1, 8, 8),
+            ]);
+            _ = tileset.copy_from(&constructed, i as u32 * 16, 0);
+        }
+        _ = tileset.save(arg.replace("assets/", "assets/gen/"));
     }
 }
