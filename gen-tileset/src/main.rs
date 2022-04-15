19077baeb4fed27c86f23523d7c8c770bc5ca82d
Russel Arbore <russel.jma@gmail.com>
2022-04-14 19:49:24 -0500

    Progress on generating tileset textures

--- gen-tileset/src/main.rs
+++ gen-tileset/src/main.rs
@@ -14,7 +14,63 @@
 
 use std::env::*;
 
-use image::io::Reader;
+use image::imageops::*;
+use image::io::*;
+use image::*;
+
+pub const LEFT_MASK: u8 = 0x01;
+pub const UP_LEFT_MASK: u8 = 0x02;
+pub const UP_MASK: u8 = 0x04;
+pub const UP_RIGHT_MASK: u8 = 0x08;
+pub const RIGHT_MASK: u8 = 0x10;
+pub const DOWN_RIGHT_MASK: u8 = 0x20;
+pub const DOWN_MASK: u8 = 0x40;
+pub const DOWN_LEFT_MASK: u8 = 0x80;
+
+#[repr(u8)]
+pub enum QuadEdgeType {
+    Clean,
+    Diag,
+    Horiz,
+    Vert,
+    Full,
+}
+
+fn num_to_edges(n: u8) -> [QuadEdgeType; 4] {
+    let mut ret = [
+        if n | UP_LEFT_MASK != 0 {
+            QuadEdgeType::Diag
+        } else {
+            QuadEdgeType::Clean
+        },
+        if n | UP_RIGHT_MASK != 0 {
+            QuadEdgeType::Diag
+        } else {
+            QuadEdgeType::Clean
+        },
+        if n | DOWN_LEFT_MASK != 0 {
+            QuadEdgeType::Diag
+        } else {
+            QuadEdgeType::Clean
+        },
+        if n | DOWN_RIGHT_MASK != 0 {
+            QuadEdgeType::Diag
+        } else {
+            QuadEdgeType::Clean
+        },
+    ];
+
+    ret
+}
+
+fn concat4(quads: [DynamicImage; 4]) -> DynamicImage {
+    let mut full = quads[0].clone().resize(16, 16, FilterType::Nearest);
+    _ = full.copy_from(&quads[0], 0, 0);
+    _ = full.copy_from(&quads[1], 8, 0);
+    _ = full.copy_from(&quads[2], 0, 8);
+    _ = full.copy_from(&quads[3], 8, 8);
+    full
+}
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -23,5 +79,8 @@ fn main() {
             .expect(&format!("Couldn't open file: {}.", arg)[..])
             .decode()
             .expect(&format!("File couldn't be coded: {}.", arg)[..]);
+        println!("{:?}", img);
+        let mut tileset = RgbaImage::new(256 * 16, 16);
+        for i in 0u8..=255u8 {}
     }
 }
